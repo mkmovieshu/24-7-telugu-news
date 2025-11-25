3a28f403ab7575e3ec0375aee7e6a14a3b91e2f1
mkmovieshu <ipsappdevelopers@gmail.com>
2025-11-25 21:05:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME", "shortnews")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "shortnews")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+PORT = int(os.getenv("PORT", "10000"))
+ADMIN_SECRET = os.getenv("ADMIN_SECRET", "change_this")
+FREE_MODE = os.getenv("FREE_MODE", "false").lower() in ("1","true","yes")
 RSS_FEEDS = [
     "https://www.eenadu.net/rss",
     "https://www.sakshi.com/rss",
