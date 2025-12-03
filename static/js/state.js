825af87656d6c3d55a4fd00e048bbc43701f29e0
mkmovieshu <ipsappdevelopers@gmail.com>
2025-12-03 13:52:58 +0530

    Update state.js

--- static/js/state.js
+++ static/js/state.js
@@ -1,26 +1,15 @@
 // static/js/state.js
 
-// అప్లికేషన్ state simple గా ఇక్కడే హ్యాండిల్ చేస్తాం
+let currentNews = null;
 
-let currentNews = null; // { id, title, summary, link, likes, dislikes }
-let isLoading = false;
-
-export function setCurrentNews(news) {
-  currentNews = news || null;
+export function setCurrentNews(item) {
+  currentNews = item;
 }
 
 export function getCurrentNews() {
   return currentNews;
 }
 
-export function getCurrentNewsId() {
-  return currentNews ? currentNews.id : null;
-}
-
-export function setLoading(loading) {
-  isLoading = loading;
-}
-
-export function getLoading() {
-  return isLoading;
+export function hasNews() {
+  return !!currentNews;
 }
