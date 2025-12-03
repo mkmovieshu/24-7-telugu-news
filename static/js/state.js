d3e5de5d661de959d45d10f2e6dff2ee60910c19
mkmovieshu <ipsappdevelopers@gmail.com>
2025-12-03 12:37:13 +0530

    Update state.js

--- static/js/state.js
+++ static/js/state.js
@@ -1,5 +1,26 @@
-// Global state for current news + animation lock
-window.NewsState = {
-  current: null,
-  isAnimating: false,
-};
+// static/js/state.js
+
+// అప్లికేషన్ state simple గా ఇక్కడే హ్యాండిల్ చేస్తాం
+
+let currentNews = null; // { id, title, summary, link, likes, dislikes }
+let isLoading = false;
+
+export function setCurrentNews(news) {
+  currentNews = news || null;
+}
+
+export function getCurrentNews() {
+  return currentNews;
+}
+
+export function getCurrentNewsId() {
+  return currentNews ? currentNews.id : null;
+}
+
+export function setLoading(loading) {
+  isLoading = loading;
+}
+
+export function getLoading() {
+  return isLoading;
+}
